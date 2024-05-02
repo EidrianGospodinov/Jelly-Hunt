dbb540dd775455f70fdf31d39af17c910f1eae16
Ntvou <Nikolaosntvouretsenski@gmail.com>
2024-05-02 01:03:12 +0100

    dialogue fixed

--- JellyHunt/Intermediate/Build/Win64/JellyHuntEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ JellyHunt/Intermediate/Build/Win64/JellyHuntEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.39.33523
-#include "C:/Users/edgos/Documents/GitHub/Jelly-Hunt/JellyHunt/Intermediate/Build/Win64/JellyHuntEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.35.32217
+#include "D:/Game Jams/Ukie GameJam/Jelly-Hunt/JellyHunt/Intermediate/Build/Win64/JellyHuntEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
